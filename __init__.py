be4851faea01aab435fe03d3aa470f66d6ca2748
A1rCHAN <airyojanchan@outlook.com>
2025-10-17 19:14:36 +0800

    Bump version to 1.0.2, fixed bugs

--- __init__.py
+++ __init__.py
@@ -35,6 +35,8 @@ WEB_DIRECTORY = "./web"
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
 
+__version__ = "1.0.2"
+
 print("==========================================================")
 print(" ")
 print(f"A1rSpace: Loaded {len(NODE_CLASS_MAPPINGS)} nodes successfully!")
